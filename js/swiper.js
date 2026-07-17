80bb65282ea7f5806f37f1e75f136b03977dabbb
beatriz-araujo <beatriz@forwwward.studio>
2026-07-17 12:52:52 +0100

    Added swiper init
    Works for multiple '.swiper' instances, applies only starting at tablet width and below

--- js/swiper.js
+++ js/swiper.js
@@ -2,68 +2,42 @@ export function swiperInit() {
 
     console.log("running swiper.js");
 
-    // Init Homepage Works Swiper
-    let hpWorksSwiper = document.querySelector('.hp_works_swiper');
-    const hpWorkSwiperSlides = document.querySelectorAll('.hp_works_slide');
+    // Init all Swipers
+    const swipersList = document.querySelectorAll('.swiper');
 
-    if (hpWorksSwiper && hpWorkSwiperSlides) {
+    if (swipersList.length > 0 && window.innerWidth <= 991) {
 
-        setTimeout(() => {
-            hpWorksSwiper = new Swiper('.hp_works_swiper', {
-                slidesPerView: 1.8,
+        swipersList.forEach((swiper) => {
+            const swiperEl = new Swiper(swiper, {
+                slidesPerView: 1.25,
                 spaceBetween: 16,
-                centeredSlides: true,
                 direction: 'horizontal',
                 loop: true,
                 autoWidth: true,
                 speed: 1000,
-                autoplay: {
-                    delay: 1800,
-                    disableOnInteraction: false,
-                },
 
                 freeMode: true,
                 freeModeMomentum: false,
                 allowTouchMove: true,
-
                 breakpoints: {
+                    // for screens 500px wide and up
+                    500: {
+                        slidesPerView: 2.25,
+                    },
                     // for screens 768px wide and up
                     768: {
-                        autoplay: {
-                            delay: 0,
-                            disableOnInteraction: false, // if true, will pause on hover
-                        },
-                        slidesPerView: 6,
-                        spaceBetween: 32,
-                        speed: 8000, // Smooth transition speed
-                        centeredSlides: false,
-                        freeMode: true,
-                        freeModeMomentum: false,
+                        slidesPerView: 3.25,
                     }
                 },
-                on: {
-                    init: function () {
-                        console.log('Swiper initialized');
-
-                        // add mouse hover listener to all slides
-                        // update swiper measurements with each hover (since they expand on hover)
-                        hpWorkSwiperSlides.forEach((slide) => {
-                            slide.addEventListener('mouseenter', () => {
-                                hpWorksSwiper.update();
-                                // hpWorksSwiper.autoplay.stop();
-                            });
-                            slide.addEventListener('mouseleave', () => {
-                                hpWorksSwiper.update();
-                                // hpWorksSwiper.autoplay.start();
-                            });
-                        });
-                    },
-                }
             });
+        });
 
-            window.addEventListener('resize', () => { hpWorksSwiper.update(); });
+        window.addEventListener('resize', () => {
+            swipersList.forEach((swiper) => {
+                swiper.update();
+            });
+        });
 
-        }, 800);
 
     }
 
